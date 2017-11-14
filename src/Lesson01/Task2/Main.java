8ba9d44df88c9d26eee232f138dac3836a302af8
Kate <kateryna.kravchenko.mail@gmail.com>
2017-11-14 12:34:01 +0200

    test task #2

--- src/Lesson01/Task2/Main.java
+++ src/Lesson01/Task2/Main.java
@@ -3,5 +3,6 @@ package Lesson01.Task2;
 public class Main {
 
     public static void main(String[] args) {
+        Saver.save();
     }
 }
--- src/Lesson01/Task2/Saver.java
+++ src/Lesson01/Task2/Saver.java
@@ -1,12 +1,41 @@
 package Lesson01.Task2;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 
-@Target(value= ElementType.METHOD)
-@Retention(value= RetentionPolicy.RUNTIME)
-public @interface Saver {
+public class Saver {
+
+    public static void save() {
+
+        Class<?> cls = TextContainer.class;
+        SaveTo saveTo = cls.getAnnotation(SaveTo.class);
+        String path = saveTo.path();
+
+        String text = "";
+
+        Field[] fields = cls.getDeclaredFields();
+        for(Field field : fields) {
+            if(field.getName().equals("text")) {
+                field.setAccessible(true);
+                try {
+                    text = field.get(text).toString();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        Method[] methods = cls.getDeclaredMethods();
+        for(Method method : methods) {
+            if(method.isAnnotationPresent(Save.class)) {
+                try {
+                    method.invoke(null, path, text);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
