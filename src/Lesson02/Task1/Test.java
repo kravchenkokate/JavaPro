528dbf3dcc7c3477369d6fce3a84b203db5c4976
Kate <kateryna.kravchenko.mail@gmail.com>
2017-11-16 15:55:45 +0200

    lesson 2 task 1

--- src/Lesson02/Task1/Test.java
+++ src/Lesson02/Task1/Test.java
@@ -7,7 +7,6 @@ import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
 import java.util.stream.Collectors;
 
 
@@ -16,15 +15,15 @@ public class Test {
     public static void execute() {
 
         try {
-            File file = new File("C:\\trains.xml");
+            File file = new File("src\\Lesson02\\Task1\\trains.xml");
             JAXBContext jaxbContext = JAXBContext.newInstance(Trains.class);
 
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
             Trains trains = (Trains) unmarshaller.unmarshal(file);
-            System.out.println("All trains.");
+            System.out.println("All trains");
             System.out.println(trains);
 
-            filterTrains(trains);
+            findTrainsByDeparture(trains, LocalTime.of(15,0), LocalTime.of(19,0));
 
             trains.add(new Train(3, "Kyiv", "Odessa", LocalDate.now(), LocalTime.of(15,30)));
             trains.add(new Train(4, "Lviv", "Kyiv", LocalDate.now(), LocalTime.of(18,45)));
@@ -32,7 +31,7 @@ public class Test {
             Marshaller marshaller = jaxbContext.createMarshaller();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             marshaller.marshal(trains, file);
-            System.out.println("New train list.");
+            System.out.println("New train list");
             marshaller.marshal(trains, System.out);
         }
         catch(JAXBException e) {
@@ -40,21 +39,11 @@ public class Test {
         }
     }
 
-    private static void filterTrains(Trains trains) {
+    private static void findTrainsByDeparture(Trains trains, LocalTime from, LocalTime to) {
 
-        System.out.println("Today from 15:00 to 19:00.");
-        LocalTime from = LocalTime.of(15,0);
-        LocalTime to = LocalTime.of(19,0);
-        List<Train> filteredTrains = trains.getTrains().stream()
+        System.out.println("Today from " + from + " to " + to);
+        trains.getTrains().stream()
                 .filter(t -> t.getDate().isEqual(LocalDate.now()) && t.getDeparture().isAfter(from) && t.getDeparture().isBefore(to))
-                .collect(Collectors.toList());
-        if(filteredTrains.size() > 0) {
-            for(Train train : filteredTrains) {
-                System.out.println(train);
-            }
-        }
-        else {
-            System.out.println("No match.");
-        }
+                .collect(Collectors.toList()).forEach(System.out::println);
     }
 }
